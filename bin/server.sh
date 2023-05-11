46a68601e24fe243a6b3f3cb41d196375e165102
Christian Mayer <christian@fox21.at>
2023-05-11 23:05:29 +0200

    Server script.

--- bin/server.sh
+++ bin/server.sh
@@ -6,4 +6,4 @@ cd "${SCRIPT_BASEDIR}/.."
 which node &> /dev/null || { echo 'ERROR: node not found in PATH'; exit 1; }
 
 #export ALLOW_SELF_CONNECT=1 # only for development
-node ./build/Server.js -c var/config1.json
+node ./build/src/app/server_app.js -c var/config1.json
