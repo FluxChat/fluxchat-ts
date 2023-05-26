65f75e66a9e949aa2dda7a0f563b1ba96c1f3a96
Christian Mayer <christian@fox21.at>
2023-05-26 19:46:45 +0200

    Clean up.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -14,17 +14,8 @@ export function passwordKeyDerivation(password: string): string {
 
 export function generateIdFromPublicKeyFile(path: string): string {
   const publicKey = crypto.createPublicKey(fs.readFileSync(path));
-  // console.warn('publicKey', publicKey, typeof publicKey);
-
   const pkExport = publicKey.export({ type: 'spki', format: 'der' });
-  // console.warn('pkExport', pkExport, typeof pkExport);
-
-  // const pkRaw = Buffer.from(pkExport.toString().split('\n').slice(1, -1).join(''), 'base64');
-  // console.warn('pkRaw', pkRaw, typeof pkRaw);
-
   const publicKeyHash = crypto.createHash('sha256').update(pkExport).digest();
-  // console.warn('publicKeyHash', publicKeyHash, typeof publicKeyHash);
-
   const publicKeyHash_bs58 = base58.encode(publicKeyHash);
 
   return `FC_${publicKeyHash_bs58}`;
